7ae9012b68100aa4bf1b05c1e08c98b186a7bd6d
samuel <sbacon803@gmail.com>
2022-07-28 20:00:29 -0600

    add dynamic singular product id get

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,13 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def show
+    #params
+    p "*" * 88
+    p params["id"]
+    p "*" * 88
+    product = Product.find_by(id: 3)
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/products" => "products#index"
+
+  get "/products/:id" => "products#show"
 end
