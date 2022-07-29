562bec961c8d9817fa326dc3d56f3138baee1141
samuel <sbacon803@gmail.com>
2022-07-28 18:34:43 -0600

    add db and some products in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+product = Product.new(name: "desk", price: 100, description: "device on which to code", image_url: "https://www.ikea.com/us/en/images/products/arkelstorp-desk-black__0735967_pe740301_s5.jpg?f=s")
+
+product.save
+
+product = Product.new(name: "computer", price: 1000, description: "device with which to code", image_url: "https://images.pexels.com/photos/38568/apple-imac-ipad-workplace-38568.jpeg?cs=srgb&dl=pexels-pixabay-38568.jpg&fm=jpg")
+
+product.save
+
+product = Product.new(name: "chair", price: 50, description: "device on which to sit", image_url: "https://m.media-amazon.com/images/I/81J5r9dANGL._SL1500_.jpg")
+
+product.save
+
+product = Product.new(name: "microphone", price: 1500, description: "device to amplify voice", image_url: "https://cdn.dpamicrophones.com/media/images/microphones/dfacto/large/2028-vocal-microphone-1-lightgrey-bg.jpg?ext=.jpg")
+
+product.save
