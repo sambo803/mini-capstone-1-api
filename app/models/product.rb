417a604b3a6ce9ee11a2b22c9337f2536af4aba9
samuel <sbacon803@gmail.com>
2022-08-03 18:52:28 -0600

    add model changes for validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ApplicationRecord
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
   end
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :price, numericality: true
+  validates :price, numericality: {greater_than 0: true}
 end
